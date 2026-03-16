092752a1b2de64ecf5761c5c63aaa012d77b6371
tyca7 <tyca7@example.com>
2026-03-16 08:14:07 +0300

    Fix Message class import collision in MeshNetworkService

--- lib/core/services/mesh_network_service.dart
+++ lib/core/services/mesh_network_service.dart
@@ -2,7 +2,7 @@ import 'dart:async';
 import 'dart:convert';
 import 'package:flutter/foundation.dart';
 import 'package:logger/logger.dart';
-import 'package:flutter_nearby_connections/flutter_nearby_connections.dart';
+import 'package:flutter_nearby_connections/flutter_nearby_connections.dart' hide Message;
 import '../models/peer.dart';
 import '../models/message.dart';
 
